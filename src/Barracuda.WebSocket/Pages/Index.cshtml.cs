de1adaf77c1e891403bd2015b8abb5b8016640b4
Abdurrahman Işık <isikabdurrahman@yahoo.com>
2020-10-02 03:12:40 +0300

    add signalr websocket demo app

---
+++ src/Barracuda.WebSocket/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Barracuda.WebSocket.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
